ec1297fa907b30533054e948c7c33fd8c04eb363
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2022-11-03 20:10:44 -0400

    updated ec2 config

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "web" {
   ami           = var.amiID
   instance_type = var.instance_type
-  monitoring = true
+  # monitoring = true
   ebs_optimized = true
-  # root_block_device {
-  #   encrypted = true
-  # }
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "AirBnB"
