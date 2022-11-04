be15b7b3f16fd1e7e01c48e9982ea12125a55298
mroluyomiosh <98926230+mroluyomiosh@users.noreply.github.com>
2022-11-03 20:15:43 -0400

    updated ec2 config

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami           = var.amiID
   instance_type = var.instance_type
-  # monitoring = true
-  ebs_optimized = true
+  monitoring = true
+  # ebs_optimized = true
   root_block_device {
     encrypted = true
   }
